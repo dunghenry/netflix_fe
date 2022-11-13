20e5e03ce72c442657e1161345459a0c278ce9a6
dunghenry <trandungksnb6930082001>
2022-11-13 22:26:13 +0700

    config webpack, firebase: register, login, logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Netflix from './pages/Netflix';
+import PageNotFound from './pages/PageNotFound';
 const App = () => {
     const dispatch = useDispatch();
-    useEffect(() => {
-        (async () => {
-            const res = await axios.get('http://localhost:4000');
-            console.log(res.data);
-        })();
-    }, []);
-    return <div>App</div>;
+    // useEffect(() => {
+    //     (async () => {
+    //         const res = await axios.get('http://localhost:4000');
+    //         console.log(res.data);
+    //     })();
+    // }, []);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route exact path="/" element={<Netflix />} />
+                <Route exact path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import styled from 'styled-components';
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
+const Login = () => {
+    const navigate = useNavigate();
+    const [formValues, setFormValues] = React.useState({
+        email: '',
+        password: '',
+    });
+    const handleSignIn = async () => {
+        try {
+            const { email, password } = formValues;
+            await signInWithEmailAndPassword(auth, email, password);
+            setFormValues({
+                email: '',
+                password: '',
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) navigate('/');
+    });
+    return (
+        <Container>
+            <BackgroundImage />
+            <div className="content">
+                <Header />
+                <div className="form-container flex column a-center j-center">
+                    <div className="form flex column a-center j-center">
+                        <div className="title">
+                            <h3>Login</h3>
+                        </div>
+                        <div className="container flex column">
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email address"
+                                value={formValues.email}
+                                onChange={(e) =>
+                                    setFormValues({
+                                        ...formValues,
+                                        [e.target.name]: e.target.value,
+                                    })
+                                }
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={formValues.password}
+                                onChange={(e) =>
+                                    setFormValues({
+                                        ...formValues,
+                                        [e.target.name]: e.target.value,
+                                    })
+                                }
+                            />
+                            <button onClick={handleSignIn}>Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default Login;
+const Container = styled.div`
+    position: relative;
+    .content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        height: 100vh;
+        width: 100vw;
+        display: grid;
+        grid-template-rows: 15vh 85vh;
+        .form-container {
+            gap: 2rem;
+            height: 85vh;
+            .form {
+                padding: 2rem;
+                background-color: #000000b0;
+                width: 25vw;
+                gap: 2rem;
+                color: white;
+                .container {
+                    gap: 2rem;
+                    input {
+                        padding: 0.5rem 1rem;
+                        width: 15rem;
+                    }
+                    button {
+                        padding: 0.5rem 1rem;
+                        background-color: #e50914;
+                        border: none;
+                        cursor: pointer;
+                        color: white;
+                        border-radius: 0.2rem;
+                        font-size: 1.05rem;
+                        font-weight: bold;
+                    }
+                }
+            }
+        }
+    }
+`;
